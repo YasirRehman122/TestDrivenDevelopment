20b728b7275e8a42b593db1b061535defdeab99a
Yasir Rehman <engnryasir122@gmail.com>
2025-10-04 10:43:29 +0500

    Refactoring tests

--- Mockito/src/test/java/com/example/learningmockito/service/UserServiceTest.java
+++ Mockito/src/test/java/com/example/learningmockito/service/UserServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.learningmockito.service;
 
 import com.example.learningmockito.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,13 +10,21 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserServiceTest {
     UserService userService = new UserServiceImpl();
 
+    String firstName = "John";
+    String lastName = "Wick";
+    String password = "12345";
+    String repeatPassword = "12345";
+
+    @BeforeEach
+    void beforeAll(){
+        firstName = "John";
+        lastName = "Wick";
+        password = "12345";
+        repeatPassword = "12345";
+    }
+
     @Test
     void testCreateUser_withValidValue_UserObjectNotNull(){
-        String firstName = "John";
-        String lastName = "Wick";
-        String password = "12345";
-        String repeatPassword = "12345";
-
         User user = userService.createUser(firstName, lastName, password, repeatPassword);
 
         assertNotNull(user);
@@ -23,9 +33,6 @@ public class UserServiceTest {
     @Test
     void testCreateUser_WhenFirstNameIsEmpty_IllegalArgumentExceptionThrown(){
         String firstName = "";
-        String lastName = "Wick";
-        String password = "12345";
-        String repeatPassword = "12345";
         String exceptionExpectedMessage= "First name can not be empty";
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
